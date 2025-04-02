b5949c195ec5b4e2ae47ac1ce3ddb8a41922ab0b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-02 21:59:32 +0400

    add dependency injection for infrastructure project

---
+++ SaaSFileManager.Infrastructure/Extensions/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaaSFileManager.Infrastructure.Data;
+
+namespace SaaSFileManager.Infrastructure.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
