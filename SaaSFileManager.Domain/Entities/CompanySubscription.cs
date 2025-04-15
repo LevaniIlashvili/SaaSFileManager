f5e08aea22ae7d920087f1ee622ec56ac2043598
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-15 21:14:20 +0400

    create company subscription entity

---
+++ SaaSFileManager.Domain/Entities/CompanySubscription.cs
@@ -0,0 +1,18 @@
+﻿namespace SaaSFileManager.Domain.Entities
+{
+    public class CompanySubscription
+    {
+        public Guid Id { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; } = default!;
+
+        public Guid SubscriptionPlanId { get; set; }
+        public SubscriptionPlan SubscriptionPlan { get; set; } = default!;
+
+        public DateTime SubscribedAt { get; set; }
+        public DateTime? CanceledAt { get; set; }
+
+        public ICollection<BillingRecord> BillingRecords { get; set; } = new List<BillingRecord>();
+    }
+}
