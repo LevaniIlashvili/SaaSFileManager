958b4b01b1c2c4bfb522596eb7763a9f796a2565
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-15 20:49:22 +0400

    create file access entity

---
+++ SaaSFileManager.Domain/Entities/FileAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace SaaSFileManager.Domain.Entities
+{
+    public class FileAccess
+    {
+        public Guid FileId { get; set; }
+        public CompanyFile File { get; set; } = default!;
+
+        public Guid EmployeeId {get;set; }
+        public Employee Employee { get; set; } = default!;
+    }
+}
