faf6ad6b3e2c195907335f4352267b68c67f76c3
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-15 21:14:38 +0400

    create billing record entity

---
+++ SaaSFileManager.Domain/Entities/BillingRecord.cs
@@ -0,0 +1,22 @@
+﻿using SaaSFileManager.Domain.common;
+
+namespace SaaSFileManager.Domain.Entities
+{
+    public class BillingRecord : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        
+        public Guid CompanySubscriptionId { get; set; }
+        public CompanySubscription CompanySubscription { get; set; } = default!;
+
+        public decimal AmountDue { get; set; }
+        public decimal? AmountPaid { get; set; }
+
+        public DateTime BillingPeriod { get; set; }
+
+        public DateTime PaidAt { get; set; }
+
+        public int TotalEmployeesThisMonth { get; set; }
+        public int TotalFilesUploadedThisMonth { get; set; }
+    }
+}
