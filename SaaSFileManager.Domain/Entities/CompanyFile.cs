19bebe3132cc02ab7a229267efc06d38075ad0dc
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-15 20:48:57 +0400

    create file entity

---
+++ SaaSFileManager.Domain/Entities/CompanyFile.cs
@@ -0,0 +1,24 @@
+﻿using SaaSFileManager.Domain.common;
+
+namespace SaaSFileManager.Domain.Entities
+{
+    public class CompanyFile : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public FileType Type { get; set; }
+
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; } = default!;
+
+        public bool IsRestricted { get; set; } = false;
+        public ICollection<FileAccess> FileAccesses { get; set; } = new List<FileAccess>();
+    }
+
+    public enum FileType
+    {
+        csv,
+        xls,
+        xlsx
+    }
+}
