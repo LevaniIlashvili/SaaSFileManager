631b66c38b2891ceffd0edd7c3c0cd4ee12cf391
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-15 20:49:51 +0400

    create subscription plan entity

---
+++ SaaSFileManager.Domain/Entities/SubscriptionPlan.cs
@@ -0,0 +1,17 @@
+﻿namespace SaaSFileManager.Domain.Entities
+{
+    public class SubscriptionPlan
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int FileLimitPerMonth { get; set; }
+        public int? UserLimit { get; set; } // null = unlimited
+        public decimal StartingPrice { get; set; } // dol $
+
+        public bool AllowExtraFiles { get; set; }
+        public bool AllowExtraUsers { get; set; }
+
+        public decimal? PricePerExtraUser { get; set; }
+        public decimal? PricePerExtraFile { get; set; }
+    }
+}
