28de3d284ac876a380a5ff52da7c77f3716a97e9
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-21 21:05:48 +0400

    add interfaces for repositories

---
+++ SaaSFileManager.Application/Contracts/Persistence/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Application.Contracts.Persistence
+{
+    public interface ICompanyRepository : IAsyncRepository<Company>
+    {
+        Task<bool> IsCompanyEmailUnique(string email);
+    }
+}
---
+++ SaaSFileManager.Application/Contracts/Persistence/IEmployeeRepository.cs
@@ -0,0 +1,8 @@
+﻿using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Application.Contracts.Persistence
+{
+    public interface IEmployeeRepository : IAsyncRepository<Employee>
+    {
+    }
+}
