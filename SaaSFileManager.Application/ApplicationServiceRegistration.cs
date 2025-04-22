9a1bd14b01c1accd1187eafa17e1f5b8a02d2fe7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-22 16:56:13 +0400

    add service extension class

---
+++ SaaSFileManager.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace SaaSFileManager.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            return services;
+        }
+    }
+}
