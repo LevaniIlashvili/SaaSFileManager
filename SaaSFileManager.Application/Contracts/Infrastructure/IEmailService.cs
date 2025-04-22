8a429eedd6dd078e59fd6e646357a311bcfee2f6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-22 16:58:04 +0400

    add interface for email service class

---
+++ SaaSFileManager.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace SaaSFileManager.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task SendActivationEmailAsync(string toEmail, string activationLink);
+    }
+}
