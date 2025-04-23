ec0964b5fcdc70fa23d6ca6b8197269083ac39aa
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-23 11:19:23 +0400

    add repositories

---
+++ SaaSFileManager.Persistence/Repositories/CompanyRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaaSFileManager.Application.Contracts.Persistence;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Persistence.Repositories
+{
+    public class CompanyRepository : BaseRepository<Company>, ICompanyRepository
+    {
+        public CompanyRepository(FileManagerDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public async Task<bool> IsCompanyEmailUnique(string email)
+        {
+            return !await _dbContext.Companies.AnyAsync(c => c.Email == email);
+        }
+    }
+}
