ac769198ab7c8037759f929668c36a931b803ff4
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-23 11:19:04 +0400

    add class for persistence service registration

---
+++ SaaSFileManager.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaaSFileManager.Application.Contracts.Persistence;
+using SaaSFileManager.Persistence.Repositories;
+
+namespace SaaSFileManager.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FileManagerDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SaaSFileManagerConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+
+            return services;
+        }
+    }
+}
