3c3b105085570dd4aef2fc3e06aa646f99283601
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-23 11:17:21 +0400

    add db context

---
+++ SaaSFileManager.Persistence/FileManagerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Persistence
+{
+    public class FileManagerDbContext : DbContext
+    {
+        public FileManagerDbContext(DbContextOptions<FileManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<CompanyFile> CompanyFiles { get; set; }
+        public DbSet<SubscriptionPlan> SubscriptionPlans { get; set; }
+        public DbSet<CompanySubscription> CompanySubscriptions { get; set; }
+        public DbSet<BillingRecord> BillingRecords { get; set; }
+        public DbSet<CompanyFileAccess> FileAccesses { get; set; }
+
+        // TODO: override SaveChangesAsync method later
+    }
+}
