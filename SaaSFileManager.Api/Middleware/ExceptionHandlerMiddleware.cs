7d2d353cf3882259b0be7a0818275f395c20115c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:34:08 +0400

    add exception handler middleware

---
+++ SaaSFileManager.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using SaaSFileManager.Application.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace SaaSFileManager.Api.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await ConvertException(context, ex);
+            }
+        }
+
+        private Task ConvertException(HttpContext context, Exception exception)
+        {
+            HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError;
+
+            context.Response.ContentType = "application/json";
+
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.ValidationErrors);
+                    break;
+                case BadRequestException badRequestException:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    result = badRequestException.Message;
+                    break;
+                case NotFoundException:
+                    httpStatusCode = HttpStatusCode.NotFound;
+                    break;
+            }
+
+            context.Response.StatusCode = (int)httpStatusCode;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ SaaSFileManager.Api/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace SaaSFileManager.Api.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
