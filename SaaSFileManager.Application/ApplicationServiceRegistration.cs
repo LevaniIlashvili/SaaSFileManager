cf9992fb02282576422e075bfda413672942ede2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:24:08 +0400

    inject application settings options

--- SaaSFileManager.Application/ApplicationServiceRegistration.cs
+++ SaaSFileManager.Application/ApplicationServiceRegistration.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SaaSFileManager.Application
 {
     public static class ApplicationServiceRegistration
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<ApplicationSettings>(configuration.GetSection("ApplicationSettings"));
+
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             return services;
         }
