41127f20d81d61be6f74142321d5c40cdcd96724
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:38:46 +0400

    add class for service registration

--- SaaSFileManager.Application/ApplicationServiceRegistration.cs
+++ SaaSFileManager.Application/ApplicationServiceRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SaaSFileManager.Application.Models.Application;
 
 namespace SaaSFileManager.Application
 {
