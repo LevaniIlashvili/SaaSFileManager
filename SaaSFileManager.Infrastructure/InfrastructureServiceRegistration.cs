aac2fa2e00a5a5c5446098b1ec6f7d09eaa74e71
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:44:59 +0400

    add class for service registration

---
+++ SaaSFileManager.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SaaSFileManager.Application.Contracts.Infrastructure;
+using SaaSFileManager.Application.Models.Mail;
+using SaaSFileManager.Infrastructure.Mail;
+
+namespace SaaSFileManager.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
