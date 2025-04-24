aa3125ca598d64a31e64e9a75dab0c4f30b11376
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:36:03 +0400

    create class for registering services and middlewares

---
+++ SaaSFileManager.Api/StartupExtensions.cs
@@ -0,0 +1,30 @@
+﻿using SaaSFileManager.Api.Middleware;
+using SaaSFileManager.Application;
+using SaaSFileManager.Infrastructure;
+using SaaSFileManager.Persistence;
+
+namespace SaaSFileManager.Api
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+            builder.Services.AddControllers();
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseCustomExceptionHandler();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
