aeeff4e3caddaf8569ebaeeb73acb5079811251a
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-24 17:33:38 +0400

    add company controller

---
+++ SaaSFileManager.Api/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SaaSFileManager.Application.Features.Companies.Commands.CreateCompany;
+
+namespace SaaSFileManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CompanyController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "CreateCompany")]
+        public async Task<IActionResult> Create([FromBody] CreateCompanyCommand createCompanyCommand)
+        {
+            await _mediator.Send(createCompanyCommand);
+
+            return Ok();
+        }
+    }
+}
