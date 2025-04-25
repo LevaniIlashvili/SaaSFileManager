114994ce6eaf6b52b5637639b296980d2df6c641
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-25 11:22:08 +0400

    add route to activate account

--- SaaSFileManager.Api/Controllers/CompanyController.cs
+++ SaaSFileManager.Api/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SaaSFileManager.Application.Features.Companies.Commands.ActivateCompany;
 using SaaSFileManager.Application.Features.Companies.Commands.CreateCompany;
 
 namespace SaaSFileManager.Api.Controllers
@@ -21,6 +22,16 @@ namespace SaaSFileManager.Api.Controllers
             await _mediator.Send(createCompanyCommand);
 
             return Ok();
+        }
+
+        [HttpGet("activate")]
+        public async Task<IActionResult> ActivateCompany([FromQuery] ActivateCompanyCommand activateCompanyCommand)
+        {
+            var activated = await _mediator.Send(activateCompanyCommand);
+
+            if (activated) return Ok ();
+
+            return BadRequest("Invalid Token");
         }
     }
 }
