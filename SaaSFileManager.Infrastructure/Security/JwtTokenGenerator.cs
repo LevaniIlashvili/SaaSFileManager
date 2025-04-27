d3ee598ab28a24027e9555548f658453d63a6e26
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-27 17:39:48 +0400

    implement jwt token generator

---
+++ SaaSFileManager.Infrastructure/Security/JwtTokenGenerator.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SaaSFileManager.Application.Contracts.Security;
+using SaaSFileManager.Application.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SaaSFileManager.Infrastructure.Security
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public JwtTokenGenerator(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(Guid companyId, string email)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, companyId.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _jwtSettings.Key));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
