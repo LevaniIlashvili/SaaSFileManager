d45ed2e6b7fd955e616fd6790b9ffe1fceb5da71
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-27 17:29:54 +0400

    change folder location from company to auth

--- SaaSFileManager.Application/Features/Companies/Commands/ActivateCompany/ActivateCompanyCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace SaaSFileManager.Application.Features.Companies.Commands.ActivateCompany
-{
-    public class ActivateCompanyCommand : IRequest<bool>
-    {
-        public string Token { get; set; }
-    }
-}
