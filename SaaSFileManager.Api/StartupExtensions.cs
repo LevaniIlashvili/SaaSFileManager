f5f658e5833eab1c6e61097d090f5198304c70ae
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-04-27 17:23:16 +0400

    add jwt authentication

--- SaaSFileManager.Api/StartupExtensions.cs
+++ SaaSFileManager.Api/StartupExtensions.cs
@@ -1,7 +1,11 @@
-﻿using SaaSFileManager.Api.Middleware;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using SaaSFileManager.Api.Middleware;
 using SaaSFileManager.Application;
+using SaaSFileManager.Application.Options;
 using SaaSFileManager.Infrastructure;
 using SaaSFileManager.Persistence;
+using System.Text;
 
 namespace SaaSFileManager.Api
 {
@@ -13,6 +17,25 @@ namespace SaaSFileManager.Api
             builder.Services.AddPersistenceServices(builder.Configuration);
             builder.Services.AddInfrastructureServices(builder.Configuration);
 
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key)),
+                    };
+                });
+
+            builder.Services.AddAuthorization();
+
             builder.Services.AddControllers();
 
             return builder.Build();
@@ -21,6 +44,9 @@ namespace SaaSFileManager.Api
         public static WebApplication ConfigurePipeline(this WebApplication app)
         {
             app.UseCustomExceptionHandler();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapControllers();
 
