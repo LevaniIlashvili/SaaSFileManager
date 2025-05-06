40f4f1af052ea49a0297fd0dd3e7ac4ca42b9191
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-06 16:03:56 +0400

    add rules for password

--- SaaSFileManager.Application/Features/Auth/Commands/RegisterCompany/RegisterCompanyCommandValidator.cs
+++ SaaSFileManager.Application/Features/Auth/Commands/RegisterCompany/RegisterCompanyCommandValidator.cs
@@ -22,8 +22,13 @@ namespace SaaSFileManager.Application.Features.Auth.Commands.CreateCompany
                 .EmailAddress().WithMessage("Invalid email format")
                 .MustAsync(CompanyEmailUnique).WithMessage("Company with this email already exists");
 
-            RuleFor(p => p.Password)
-                .MinimumLength(8);
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"[0-9]").WithMessage("Password must contain at least one number.")
+                .Matches(@"[\W]").WithMessage("Password must contain at least one special character.");
 
             RuleFor(p => p.Country)
                 .NotNull()
