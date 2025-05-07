4c5b74bafa6601b0a1a62944f6be5ffb2850ffbc
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-07 11:22:35 +0400

    add password hasher service

---
+++ SaaSFileManager.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using SaaSFileManager.Application.Contracts.Infrastructure;
+
+namespace SaaSFileManager.Infrastructure.Security
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
