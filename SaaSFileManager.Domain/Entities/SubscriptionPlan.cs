347cb6a0faafbc1251fbfeb50fe1a32509ec0662
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-08 16:28:40 +0400

    change subscription plan id from int to guid

--- SaaSFileManager.Domain/Entities/SubscriptionPlan.cs
+++ SaaSFileManager.Domain/Entities/SubscriptionPlan.cs
@@ -2,16 +2,14 @@
 {
     public class SubscriptionPlan
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int FileLimitPerMonth { get; set; }
         public int? UserLimit { get; set; } // null = unlimited
+        public decimal PricePerUser { get;set; }
         public decimal StartingPrice { get; set; } // dol $
 
         public bool AllowExtraFiles { get; set; }
-        public bool AllowExtraUsers { get; set; }
-
-        public decimal? PricePerExtraUser { get; set; }
         public decimal? PricePerExtraFile { get; set; }
     }
 }
