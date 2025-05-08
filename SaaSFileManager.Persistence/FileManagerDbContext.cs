a12dde0f1ac78251afbd9d7226faec8aa50de54d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-08 16:29:51 +0400

    seed subscription plans

--- SaaSFileManager.Persistence/FileManagerDbContext.cs
+++ SaaSFileManager.Persistence/FileManagerDbContext.cs
@@ -25,6 +25,37 @@ namespace SaaSFileManager.Persistence
                 .WithMany()
                 .HasForeignKey(fa => fa.FileId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<SubscriptionPlan>().HasData(
+                    new SubscriptionPlan
+                    {
+                        Id = new Guid("6523feb5-b2e5-44f0-86b1-f3029a966310"),
+                        Name = "Free",
+                        FileLimitPerMonth = 10,
+                        UserLimit = 1,
+                        StartingPrice = 0,
+                        AllowExtraFiles = false,
+                    },
+                    new SubscriptionPlan
+                    {
+                        Id = new Guid("12455643-90a2-4477-9aaa-fa212163bb5f"),
+                        Name = "Basic",
+                        FileLimitPerMonth = 100,
+                        UserLimit = 10,
+                        StartingPrice = 0,
+                        PricePerUser = 5,
+                        AllowExtraFiles = false,
+                    },
+                    new SubscriptionPlan
+                    {
+                        Id = new Guid("3ea029f3-bdd3-4ca4-9331-78f269d0d742"),
+                        Name = "Premium",
+                        FileLimitPerMonth = 1000,
+                        UserLimit = null,
+                        StartingPrice = 300,
+                        AllowExtraFiles = true,
+                        PricePerExtraFile = 0.5M
+                    });
         }
 
         public DbSet<Company> Companies { get; set; }
