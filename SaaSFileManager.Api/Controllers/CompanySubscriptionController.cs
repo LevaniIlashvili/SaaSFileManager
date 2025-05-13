9f78bca358c5ad8e8cb239e4b8d76649b03ae434
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-13 16:32:05 +0400

    implement controller for company subscriptions

---
+++ SaaSFileManager.Api/Controllers/CompanySubscriptionController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SaaSFileManager.Application.Features.CompanySubscriptions.Commands.ChooseSubscriptionPlan;
+using SaaSFileManager.Application.Features.CompanySubscriptions.Queries.GetCompanyCurrentSubscription;
+
+namespace SaaSFileManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanySubscriptionController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CompanySubscriptionController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("{SubscriptionPlanId}")]
+        public async Task<IActionResult> ChooseSubscription([FromRoute] ChooseSubscriptionPlanCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CompanySubscriptionVM>> GetCurrentSubscription()
+        {
+            var query = new GetCompanyCurrentSubscriptionQuery();
+            return await _mediator.Send(query);
+        }
+        
+    }
+}
