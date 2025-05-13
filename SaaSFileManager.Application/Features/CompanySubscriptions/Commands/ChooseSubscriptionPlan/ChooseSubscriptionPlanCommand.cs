0a7174b3a1586fa083461bbb1d495be7ae9f85fa
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-13 16:32:58 +0400

    add functionality to choose subscription

---
+++ SaaSFileManager.Application/Features/CompanySubscriptions/Commands/ChooseSubscriptionPlan/ChooseSubscriptionPlanCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SaaSFileManager.Application.Features.CompanySubscriptions.Commands.ChooseSubscriptionPlan
+{
+    public class ChooseSubscriptionPlanCommand : IRequest
+    {
+        public string SubscriptionPlanId { get; set; }
+    }
+}
---
+++ SaaSFileManager.Application/Features/CompanySubscriptions/Commands/ChooseSubscriptionPlan/ChooseSubscriptionPlanCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using SaaSFileManager.Application.Contracts.Infrastructure;
+using SaaSFileManager.Application.Contracts.Persistence;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Application.Features.CompanySubscriptions.Commands.ChooseSubscriptionPlan
+{
+    public class ChooseSubscriptionPlanCommandHandler : IRequestHandler<ChooseSubscriptionPlanCommand>
+    {
+        private readonly ICompanySubscriptionRepository _companySubscriptionRepository;
+        private readonly IAsyncRepository<SubscriptionPlan> _subscriptionPlanRepository;
+        private readonly ILoggedInUserService _loggedInUserService;
+
+        public ChooseSubscriptionPlanCommandHandler(ICompanySubscriptionRepository companySubscriptionRepository, IAsyncRepository<SubscriptionPlan> subscriptionPlanRepository, ILoggedInUserService loggedInUserService)
+        {
+            _companySubscriptionRepository = companySubscriptionRepository;
+            _subscriptionPlanRepository = subscriptionPlanRepository;
+            _loggedInUserService = loggedInUserService;
+        }
+
+        public async Task Handle(ChooseSubscriptionPlanCommand command, CancellationToken token)
+        {
+            var subscriptionPlan = await _subscriptionPlanRepository.GetByIdAsync(Guid.Parse(command.SubscriptionPlanId));
+            if (subscriptionPlan == null)
+            {
+                throw new Exceptions.NotFoundException($"Subscription plan with ID {command.SubscriptionPlanId} not found.");
+            }
+
+            var companyId = _loggedInUserService.UserId;
+
+            var currentSubscription = await _companySubscriptionRepository.GetActiveSubscriptionByCompanyId(Guid.Parse(companyId));
+
+            if (currentSubscription != null)
+            {
+                if (currentSubscription.SubscriptionPlanId.ToString() == command.SubscriptionPlanId)
+                {
+                    throw new Exceptions.ConflictException("Company is already subscribed to this plan.");
+                }
+
+                currentSubscription.CanceledAt = DateTime.Now;
+                await _companySubscriptionRepository.UpdateAsync(currentSubscription);
+            }
+
+            var subscription = new CompanySubscription
+            {
+                Id = Guid.NewGuid(),
+                CompanyId = Guid.Parse(companyId),
+                SubscriptionPlanId = Guid.Parse(command.SubscriptionPlanId),
+                SubscribedAt = DateTime.UtcNow,
+                CanceledAt = null
+            };
+            await _companySubscriptionRepository.AddAsync(subscription);
+        }
+    }
+}
