d3b7042f9e00fd7e11691f714fbaa20846a5e066
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-13 16:31:39 +0400

    add exception for conflicts

---
+++ SaaSFileManager.Application/Exceptions/ConflictException.cs
@@ -0,0 +1,9 @@
+﻿namespace SaaSFileManager.Application.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message)
+        {     
+        }
+    }
+}
