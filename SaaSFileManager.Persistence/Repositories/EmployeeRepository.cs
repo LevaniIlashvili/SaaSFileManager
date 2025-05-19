6b7b3001f0015af045d90be1d0f06fcdb6a5fa6d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-19 20:41:15 +0400

    add employee repository

---
+++ SaaSFileManager.Persistence/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaaSFileManager.Application.Contracts.Persistence;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Persistence.Repositories
+{
+    public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(FileManagerDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<bool> IsEmployeeEmailUnique(string email)
+        {
+            return !await _dbContext.Employees.AnyAsync(e => e.Email == email);
+        }
+    }
+}
