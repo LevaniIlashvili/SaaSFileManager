50e1510e609fca38a668a3d0bc5d082129e50cb7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-19 20:43:47 +0400

    implement functionality for adding employee to company

---
+++ SaaSFileManager.Application/Features/Companies/Commands/AddEmployee/AddEmployeeCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SaaSFileManager.Application.Features.Companies.Commands.AddEmployee
+{
+    public class AddEmployeeCommand : IRequest<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ SaaSFileManager.Application/Features/Companies/Commands/AddEmployee/AddEmployeeCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace SaaSFileManager.Application.Features.Companies.Commands.AddEmployee
+{
+    public class AddEmployeeCommandValidator : AbstractValidator<AddEmployeeCommand>
+    {
+        public AddEmployeeCommandValidator()
+        {
+            RuleFor(p => p.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.LastName)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters.");
+
+            RuleFor(p => p.Email)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .NotNull()
+                .EmailAddress().WithMessage("Invalid email format");
+        }
+    }
+}
