c68f76383874fd9d0a32675e250c225b23d5ce35
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-19 20:44:09 +0400

    update employee entity to have activation token property

---
+++ SaaSFileManager.Persistence/Migrations/20250519083209_AddActivationTokenToEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaaSFileManager.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActivationTokenToEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ActivationToken",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActivationToken",
+                table: "Employees");
+        }
+    }
+}
