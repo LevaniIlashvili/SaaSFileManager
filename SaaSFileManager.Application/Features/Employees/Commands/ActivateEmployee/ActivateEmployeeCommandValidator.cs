6794dd1d023812eeb77a2a994293cde65f1020d6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-22 10:26:13 +0400

    add functionality to activate employee account

---
+++ SaaSFileManager.Application/Features/Employees/Commands/ActivateEmployee/ActivateEmployeeCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace SaaSFileManager.Application.Features.Employees.Commands.ActivateEmployee
+{
+    public class ActivateEmployeeCommandValidator : AbstractValidator<ActivateEmployeeCommand>
+    {
+        public ActivateEmployeeCommandValidator()
+        {
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches(@"[0-9]").WithMessage("Password must contain at least one number.")
+                .Matches(@"[\W]").WithMessage("Password must contain at least one special character.");
+        }
+    }
+}
