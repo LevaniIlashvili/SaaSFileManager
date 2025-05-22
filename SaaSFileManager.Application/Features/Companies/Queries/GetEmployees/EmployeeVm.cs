5a79f4c63072f9228928d51c55dd0a09aa7a8efe
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-05-22 11:30:00 +0400

    add functionality to get employees

---
+++ SaaSFileManager.Application/Features/Companies/Queries/GetEmployees/EmployeeVm.cs
@@ -0,0 +1,11 @@
+﻿namespace SaaSFileManager.Application.Features.Companies.Queries.GetEmployees
+{
+    public class EmployeeVm
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool IsActivated { get; set; } 
+    }
+}
---
+++ SaaSFileManager.Application/Features/Companies/Queries/GetEmployees/GetEmployeesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using SaaSFileManager.Application.Contracts.Persistence;
+
+namespace SaaSFileManager.Application.Features.Companies.Queries.GetEmployees
+{
+    public class GetEmployeesQueryHandler : IRequestHandler<GetEmployeesQuery, List<EmployeeVm>>
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public GetEmployeesQueryHandler(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EmployeeVm>> Handle(GetEmployeesQuery query, CancellationToken cancellationToken)
+        {
+            var employees = await _employeeRepository.ListAllAsync();
+
+            return _mapper.Map<List<EmployeeVm>>(employees);
+        }
+    }
+}
