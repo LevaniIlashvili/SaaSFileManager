ebe28c25e66a2506bf85d58435aa23da3cca2e90
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-22 16:40:23 +0400

    add file repository

---
+++ SaaSFileManager.Application/Contracts/Persistence/IFileRepository.cs
@@ -0,0 +1,11 @@
+﻿using SaaSFileManager.Application.Contracts.Persistence;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Application.Contracts.Infrastructure
+{
+    public interface IFileRepository : IAsyncRepository<CompanyFile>
+    {
+        Task<List<CompanyFile>> GetFilesVisibleToEmployee(Guid employeeId);
+        Task<List<CompanyFile>> GetFiles();
+    }
+}
---
+++ SaaSFileManager.Persistence/Repositories/FileRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaaSFileManager.Application.Contracts.Infrastructure;
+using SaaSFileManager.Domain.Entities;
+
+namespace SaaSFileManager.Persistence.Repositories
+{
+    public class FileRepository : BaseRepository<CompanyFile>, IFileRepository
+    {
+        public FileRepository(FileManagerDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<CompanyFile>> GetFilesVisibleToEmployee(Guid employeeId)
+        {
+            return await _dbContext.CompanyFiles.Where(f => f.IsRestricted == false || f.FileAccesses.Any(f => f.EmployeeId == employeeId)).ToListAsync();
+        }
+
+        public async Task<List<CompanyFile>> GetFiles()
+        {
+            return await _dbContext.CompanyFiles.ToListAsync();
+        }
+    }
+}
