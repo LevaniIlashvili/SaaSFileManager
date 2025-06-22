72a714a654b3f381a815c83024277a94cc765e56
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-06-22 16:52:15 +0400

    add methods

--- SaaSFileManager.Application/Contracts/Infrastructure/IFileStorageService.cs
+++ SaaSFileManager.Application/Contracts/Infrastructure/IFileStorageService.cs
@@ -3,5 +3,7 @@
     public interface IFileStorageService
     {
         Task<string> SaveFileAsync(string fileName, byte[] content);
+        Task<byte[]> GetFileAsync(string storagePath);
+        Task DeleteFileAsync(string relativePath);
     }
 }
